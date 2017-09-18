43ce7049c47e1696e59fd81ab6cc39f82e78e991
amatranga <amatranga892@gmail.com>
2017-09-18 15:12:41 -0700

    Implements index.jsx file

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PasswordDashboard from './components/PasswordDashboard';
+
+ReactDOM.render(<PasswordDashboard />, document.getElementById('app'));
